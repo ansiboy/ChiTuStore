fd8ef2639bc85f89f5a7ce0e69b003635eac40b3
Shu Mai <ansiboy@163.com>
2016-02-13 20:20:40 +0800

    no message

--- Web/App/Core/Carousel.ts
+++ Web/App/Core/Carousel.ts
@@ -2,15 +2,17 @@ import Hammer = require('hammer');
 import move = require('move'); // 说明：使用 move.js 框加，比直接使用 webkitTransform 样式要高效，后者在 iphone 真机上会卡。
 
 //TODO: 活动圆点的显示
+//TODO: 如果 items 为0,或者为 1 的情况。
 
 var animateTime = 600;//ms
 class Carousel {
 
     private playTimeId: number = 0;// 0 为停止中，－1 为已停止，非 0 为播放中。
     private playing = false;
+    private paned = false;
     private window_width = $(window).width();
     private active_position: number; // 记录活动页的位置（按页面的百份比）
-    private active_index = 0;
+    private active_index: number;
     private items: HTMLElement[];
 
     constructor(element: HTMLElement) {
@@ -19,81 +21,86 @@ class Carousel {
 
         var $carousel = $(element);
         this.items = $carousel.find('.item').toArray();
+        this.active_index = $carousel.find('.active').index();// || 0;
+        this.active_index = this.active_index >= 0 ? this.active_index : 0;
 
         var hammer = new Hammer(element);
         hammer.get('pan').set({
             direction: Hammer.DIRECTION_HORIZONTAL,
         });
 
-
-        
-        //TODO: 如果 items 为0,或者为 1 的情况。
-
         $(this.activeItem()).addClass('active');
-        hammer.on('panstart', (e: PanEvent) => {
-            this.stop();
-
-        }).on('panmove', (e: PanEvent) => {
-
-            var percent_position = Math.floor(e.deltaX / $(window).width() * 100);
-            if (this.active_position == percent_position || this.playing == true) {
-                return;
-            }
-
-            move(this.activeItem()).x(e.deltaX).duration(0).end();
-            this.active_position = percent_position;
-
-            if (percent_position < 0) {
-                this.nextItem().className = 'item next';
-                move(this.nextItem()).x(this.window_width + e.deltaX).duration(0).end();
-            }
-            else if (percent_position > 0) {
-                this.prevItem().className = 'item prev';
-                move(this.prevItem()).x(e.deltaX - this.window_width).duration(0).end();
-            }
-
-        }).on('panend', () => {
-            var duration_time = 200;
-            var p = 50;
-            if (this.active_position > 0 && this.active_position >= p) {
-                move(this.activeItem()).x(this.window_width).duration(duration_time).end();
-                move(this.prevItem()).x(0).duration(duration_time).end();
-
-                window.setTimeout(() => {
-                    $(this.prevItem()).removeClass('prev next').addClass('active');
-                    $(this.activeItem()).removeClass('active');
-                    this.decreaseActiveIndex();
-
-                }, duration_time);
-            }
-            else if (this.active_position > 0 && this.active_position < p) {
-                move(this.activeItem()).x(0).duration(duration_time).end();
-                move(this.prevItem()).x(0 - this.window_width).duration(duration_time).end();
-            }
-            else if (this.active_position <= 0 - p) {
-                move(this.activeItem()).x(0 - this.window_width).duration(duration_time).end();
-                move(this.nextItem()).x(0).duration(duration_time).end();
-
-                window.setTimeout(() => {
-                    $(this.nextItem()).removeClass('prev next').addClass('active');
-                    $(this.activeItem()).removeClass('active');
-                    this.increaseActiveIndex();
-
-                }, duration_time);
-            }
-            else {
-                // 取消滑动到下一页，还原回原来的位置。
-                move(this.activeItem()).x(0).duration(duration_time).end();
-                move(this.nextItem()).x(this.window_width).duration(duration_time).end();
-            }
-
-            window.setTimeout(() => {
-                this.play();
-            }, duration_time + 200);
-        });
+        hammer.on('panstart', (e: PanEvent) => this.panstart(e))
+            .on('panmove', (e: PanEvent) => this.panmove(e))
+            .on('panend', (e: PanEvent) => this.panend(e));
 
         this.play();
-        // })
+    }
+
+    private panstart(e: PanEvent) {
+        this.stop();
+    }
+    private panmove(e: PanEvent) {
+        var percent_position = Math.floor(e.deltaX / $(window).width() * 100);
+        if (this.active_position == percent_position || this.playing == true) {
+            return;
+        }
+
+        this.paned = true;
+        move(this.activeItem()).x(e.deltaX).duration(0).end();
+        this.active_position = percent_position;
+
+        if (percent_position < 0) {
+            this.nextItem().className = 'item next';
+            move(this.nextItem()).x(this.window_width + e.deltaX).duration(0).end();
+        }
+        else if (percent_position > 0) {
+            this.prevItem().className = 'item prev';
+            move(this.prevItem()).x(e.deltaX - this.window_width).duration(0).end();
+        }
+    }
+    private panend(e: PanEvent) {
+        if (this.paned == false)
+            return;
+
+        this.paned = false;
+        var duration_time = 200;
+        var p = 50;
+        if (this.active_position > 0 && this.active_position >= p) {
+            move(this.activeItem()).x(this.window_width).duration(duration_time).end();
+            move(this.prevItem()).x(0).duration(duration_time).end();
+
+            window.setTimeout(() => {
+                $(this.prevItem()).removeClass('prev next').addClass('active');
+                $(this.activeItem()).removeClass('active');
+                this.decreaseActiveIndex();
+
+            }, duration_time);
+        }
+        else if (this.active_position > 0 && this.active_position < p) {
+            move(this.activeItem()).x(0).duration(duration_time).end();
+            move(this.prevItem()).x(0 - this.window_width).duration(duration_time).end();
+        }
+        else if (this.active_position <= 0 - p) {
+            move(this.activeItem()).x(0 - this.window_width).duration(duration_time).end();
+            move(this.nextItem()).x(0).duration(duration_time).end();
+
+            window.setTimeout(() => {
+                $(this.nextItem()).removeClass('prev next').addClass('active');
+                $(this.activeItem()).removeClass('active');
+                this.increaseActiveIndex();
+
+            }, duration_time);
+        }
+        else {
+            // 取消滑动到下一页，还原回原来的位置。
+            move(this.activeItem()).x(0).duration(duration_time).end();
+            move(this.nextItem()).x(this.window_width).duration(duration_time).end();
+        }
+
+        window.setTimeout(() => {
+            this.play();
+        }, duration_time + 200);
     }
     private increaseActiveIndex() {
         this.active_index = this.active_index + 1;
@@ -128,17 +135,17 @@ class Carousel {
 
         return this.items[nextIndex];
     }
-    prevItem() {
+    private prevItem() {
         var prevIndex = this.active_index - 1;
         if (prevIndex < 0)
             prevIndex = this.items.length - 1;
 
         return this.items[prevIndex];
     }
-    activeItem() {
+    private activeItem() {
         return this.items[this.active_index];
     }
-    moveNext() {
+    private moveNext() {
         if (this.playTimeId == 0)
             return;
 
@@ -179,7 +186,7 @@ class Carousel {
 
     }
 
-    movePrev() {
+    private movePrev() {
         if (this.playTimeId == 0)
             return;
 
@@ -217,7 +224,7 @@ class Carousel {
 
     }
 
-    stop() {
+    private stop() {
         if (this.playTimeId == 0) {
             return;
         }
@@ -227,7 +234,7 @@ class Carousel {
         this.playTimeId = 0;
     }
 
-    play() {
+    private play() {
         if (this.playTimeId != 0)
             return;
 
