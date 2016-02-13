b403c5579b4c1f73f9294478269012ccbb9103e8
Shu Mai <ansiboy@163.com>
2016-02-13 20:18:50 +0800

    fixe 一个快速划屏的时候，会出现白屏的bug。

--- Build/App/Core/Carousel.js
+++ Build/App/Core/Carousel.js
@@ -5,69 +5,81 @@ define(["require", "exports", 'hammer', 'move'], function (require, exports, Ham
             var _this = this;
             this.playTimeId = 0;
             this.playing = false;
+            this.paned = false;
             this.window_width = $(window).width();
-            this.active_index = 0;
             if (element == null)
                 throw chitu.Errors.argumentNull('element');
             var $carousel = $(element);
             this.items = $carousel.find('.item').toArray();
+            this.active_index = $carousel.find('.active').index();
+            this.active_index = this.active_index >= 0 ? this.active_index : 0;
             var hammer = new Hammer(element);
             hammer.get('pan').set({
                 direction: Hammer.DIRECTION_HORIZONTAL,
             });
             $(this.activeItem()).addClass('active');
-            hammer.on('panstart', function (e) {
-                _this.stop();
-            }).on('panmove', function (e) {
-                var percent_position = Math.floor(e.deltaX / $(window).width() * 100);
-                if (_this.active_position == percent_position || _this.playing == true) {
-                    return;
-                }
-                move(_this.activeItem()).x(e.deltaX).duration(0).end();
-                _this.active_position = percent_position;
-                if (percent_position < 0) {
-                    _this.nextItem().className = 'item next';
-                    move(_this.nextItem()).x(_this.window_width + e.deltaX).duration(0).end();
-                }
-                else if (percent_position > 0) {
-                    _this.prevItem().className = 'item prev';
-                    move(_this.prevItem()).x(e.deltaX - _this.window_width).duration(0).end();
-                }
-            }).on('panend', function () {
-                var duration_time = 200;
-                var p = 50;
-                if (_this.active_position > 0 && _this.active_position >= p) {
-                    move(_this.activeItem()).x(_this.window_width).duration(duration_time).end();
-                    move(_this.prevItem()).x(0).duration(duration_time).end();
-                    window.setTimeout(function () {
-                        $(_this.prevItem()).removeClass('prev next').addClass('active');
-                        $(_this.activeItem()).removeClass('active');
-                        _this.decreaseActiveIndex();
-                    }, duration_time);
-                }
-                else if (_this.active_position > 0 && _this.active_position < p) {
-                    move(_this.activeItem()).x(0).duration(duration_time).end();
-                    move(_this.prevItem()).x(0 - _this.window_width).duration(duration_time).end();
-                }
-                else if (_this.active_position <= 0 - p) {
-                    move(_this.activeItem()).x(0 - _this.window_width).duration(duration_time).end();
-                    move(_this.nextItem()).x(0).duration(duration_time).end();
-                    window.setTimeout(function () {
-                        $(_this.nextItem()).removeClass('prev next').addClass('active');
-                        $(_this.activeItem()).removeClass('active');
-                        _this.increaseActiveIndex();
-                    }, duration_time);
-                }
-                else {
-                    move(_this.activeItem()).x(0).duration(duration_time).end();
-                    move(_this.nextItem()).x(_this.window_width).duration(duration_time).end();
-                }
-                window.setTimeout(function () {
-                    _this.play();
-                }, duration_time + 200);
-            });
+            hammer.on('panstart', function (e) { return _this.panstart(e); })
+                .on('panmove', function (e) { return _this.panmove(e); })
+                .on('panend', function (e) { return _this.panend(e); });
             this.play();
         }
+        Carousel.prototype.panstart = function (e) {
+            this.stop();
+        };
+        Carousel.prototype.panmove = function (e) {
+            var percent_position = Math.floor(e.deltaX / $(window).width() * 100);
+            if (this.active_position == percent_position || this.playing == true) {
+                return;
+            }
+            this.paned = true;
+            move(this.activeItem()).x(e.deltaX).duration(0).end();
+            this.active_position = percent_position;
+            if (percent_position < 0) {
+                this.nextItem().className = 'item next';
+                move(this.nextItem()).x(this.window_width + e.deltaX).duration(0).end();
+            }
+            else if (percent_position > 0) {
+                this.prevItem().className = 'item prev';
+                move(this.prevItem()).x(e.deltaX - this.window_width).duration(0).end();
+            }
+        };
+        Carousel.prototype.panend = function (e) {
+            var _this = this;
+            if (this.paned == false)
+                return;
+            this.paned = false;
+            var duration_time = 200;
+            var p = 50;
+            if (this.active_position > 0 && this.active_position >= p) {
+                move(this.activeItem()).x(this.window_width).duration(duration_time).end();
+                move(this.prevItem()).x(0).duration(duration_time).end();
+                window.setTimeout(function () {
+                    $(_this.prevItem()).removeClass('prev next').addClass('active');
+                    $(_this.activeItem()).removeClass('active');
+                    _this.decreaseActiveIndex();
+                }, duration_time);
+            }
+            else if (this.active_position > 0 && this.active_position < p) {
+                move(this.activeItem()).x(0).duration(duration_time).end();
+                move(this.prevItem()).x(0 - this.window_width).duration(duration_time).end();
+            }
+            else if (this.active_position <= 0 - p) {
+                move(this.activeItem()).x(0 - this.window_width).duration(duration_time).end();
+                move(this.nextItem()).x(0).duration(duration_time).end();
+                window.setTimeout(function () {
+                    $(_this.nextItem()).removeClass('prev next').addClass('active');
+                    $(_this.activeItem()).removeClass('active');
+                    _this.increaseActiveIndex();
+                }, duration_time);
+            }
+            else {
+                move(this.activeItem()).x(0).duration(duration_time).end();
+                move(this.nextItem()).x(this.window_width).duration(duration_time).end();
+            }
+            window.setTimeout(function () {
+                _this.play();
+            }, duration_time + 200);
+        };
         Carousel.prototype.increaseActiveIndex = function () {
             this.active_index = this.active_index + 1;
             if (this.active_index > this.items.length - 1)
