4adfcb5b712100686aeaad5f249aa3797e604f6c
yunde <yunde@yunde-mac-mini.local>
2016-06-06 16:42:35 +0800

    使用封装好的 ScrollViewGesture 插件

--- src/App/Module/Home/Product.ts
+++ src/App/Module/Home/Product.ts
@@ -8,6 +8,7 @@ import TopBar = require('UI/TopBar');
 import ProductPanel = require('Module/Home/Product/ProductPanel');
 import ProductDetailPanel = require('Module/Home/Product/ProductDetailPanel');
 import auth = require('Services/Auth');
+import ScrollViewGesture = require('Core/ScrollViewGesture');
 
 var services = window['services'];
 
@@ -102,81 +103,81 @@ class ProductPage extends chitu.Page {
         var active_item_pos = 0;
         var next_item_pos = container_width;
         var prev_item_pos = 0 - container_width;
+        new ScrollViewGesture(this.findControl<chitu.ScrollView>('product'));
+        // var pan = page.container.gesture.createPan(page.container.element);
+        // pan.start = (e: PanEvent) => {
+        //     console.log('start');
+        //     $active_item = $(page.element).find('scroll-view.active');
+        //     if ($active_item.length == 0 || chitu.ScrollView.scrolling) {
+        //         return false;
+        //     }
 
-        var pan = page.container.gesture.createPan(page.container.element);
-        pan.start = (e: PanEvent) => {
-            console.log('start');
-            $active_item = $(page.element).find('scroll-view.active');
-            if ($active_item.length == 0 || chitu.ScrollView.scrolling) {
-                return false;
-            }
+        //     //==================================================
+        //     // 说明：计算角度，超过了水平滑动角度，则认为不是水平滑动。
+        //     var d = Math.atan(Math.abs(e.deltaY / e.deltaX)) / 3.14159265 * 180;
+        //     if (d > 20)
+        //         return false;
+        //     //==================================================
 
-            //==================================================
-            // 说明：计算角度，超过了水平滑动角度，则认为不是水平滑动。
-            var d = Math.atan(Math.abs(e.deltaY / e.deltaX)) / 3.14159265 * 180;
-            if (d > 20)
-                return false;
-            //==================================================
+        //     $next_item = $active_item.next('scroll-view');
+        //     $prev_item = $active_item.prev('scroll-view');
 
-            $next_item = $active_item.next('scroll-view');
-            $prev_item = $active_item.prev('scroll-view');
+        //     var index = $(page.element).find('scroll-view').index($active_item);
+        //     var started = (index == 0 && (e.direction & Hammer.DIRECTION_LEFT) == Hammer.DIRECTION_LEFT) || (index == 1) ||
+        //         (index == 2 && (e.direction & Hammer.DIRECTION_RIGHT) == Hammer.DIRECTION_RIGHT);
+        //     return started;
+        // };
 
-            var index = $(page.element).find('scroll-view').index($active_item);
-            var started = (index == 0 && (e.direction & Hammer.DIRECTION_LEFT) == Hammer.DIRECTION_LEFT) || (index == 1) ||
-                (index == 2 && (e.direction & Hammer.DIRECTION_RIGHT) == Hammer.DIRECTION_RIGHT);
-            return started;
-        };
+        // pan.left = (e: PanEvent) => {
+        //     move($active_item[0]).x(active_item_pos + e.deltaX).duration(0).end();
+        //     if ($next_item.length > 0)
+        //         move($next_item[0]).x(next_item_pos + e.deltaX).duration(0).end();
 
-        pan.left = (e: PanEvent) => {
-            move($active_item[0]).x(active_item_pos + e.deltaX).duration(0).end();
-            if ($next_item.length > 0)
-                move($next_item[0]).x(next_item_pos + e.deltaX).duration(0).end();
+        //     if ($prev_item.length > 0)
+        //         move($prev_item[0]).x(prev_item_pos + e.deltaX).duration(0).end();
 
-            if ($prev_item.length > 0)
-                move($prev_item[0]).x(prev_item_pos + e.deltaX).duration(0).end();
+        // };
 
-        };
+        // pan.right = (e: PanEvent) => {
+        //     move($active_item[0]).x(active_item_pos + e.deltaX).duration(0).end();
+        //     if ($next_item.length > 0)
+        //         move($next_item[0]).x(next_item_pos + e.deltaX).duration(0).end();
 
-        pan.right = (e: PanEvent) => {
-            move($active_item[0]).x(active_item_pos + e.deltaX).duration(0).end();
-            if ($next_item.length > 0)
-                move($next_item[0]).x(next_item_pos + e.deltaX).duration(0).end();
+        //     if ($prev_item.length > 0)
+        //         move($prev_item[0]).x(prev_item_pos + e.deltaX).duration(0).end();
 
-            if ($prev_item.length > 0)
-                move($prev_item[0]).x(prev_item_pos + e.deltaX).duration(0).end();
+        //     //page.container.gesture.prevent.pan(Hammer.DIRECTION_RIGHT);
+        // }
 
-            page.container.gesture.prevent.pan(Hammer.DIRECTION_RIGHT);
-        }
+        // pan.end = (e: PanEvent) => {
+        //     console.log('end');
+        //     if (Math.abs(e.deltaX) / container_width < 0.5) {
+        //         move($active_item[0]).x(active_item_pos).end();
+        //         if ($next_item.length > 0)
+        //             move($next_item[0]).x(next_item_pos).end();
+        //         if ($prev_item.length > 0)
+        //             move($prev_item[0]).x(prev_item_pos).end();
 
-        pan.end = (e: PanEvent) => {
-            console.log('end');
-            if (Math.abs(e.deltaX) / container_width < 0.5) {
-                move($active_item[0]).x(active_item_pos).end();
-                if ($next_item.length > 0)
-                    move($next_item[0]).x(next_item_pos).end();
-                if ($prev_item.length > 0)
-                    move($prev_item[0]).x(prev_item_pos).end();
+        //         return;
+        //     }
 
-                return;
-            }
+        //     if (e.deltaX < 0 && $next_item.length > 0) { // 向左移动
+        //         move($active_item[0]).x(prev_item_pos).end();
+        //         move($next_item[0]).x(active_item_pos).end();
+        //         $next_item.addClass('active');
+        //         $active_item.removeClass('active');
+        //     }
+        //     else if (e.deltaX > 0 && $prev_item.length > 0) { // 向右移动
+        //         move($active_item[0]).x(next_item_pos).end();
+        //         move($prev_item[0]).x(active_item_pos).end();
+        //         $prev_item.addClass('active');
+        //         $active_item.removeClass('active');
+        //     }
 
-            if (e.deltaX < 0 && $next_item.length > 0) { // 向左移动
-                move($active_item[0]).x(prev_item_pos).end();
-                move($next_item[0]).x(active_item_pos).end();
-                $next_item.addClass('active');
-                $active_item.removeClass('active');
-            }
-            else if (e.deltaX > 0 && $prev_item.length > 0) { // 向右移动
-                move($active_item[0]).x(next_item_pos).end();
-                move($prev_item[0]).x(active_item_pos).end();
-                $prev_item.addClass('active');
-                $active_item.removeClass('active');
-            }
-
-            $active_item = $(page.element).find('scroll-view.active');
-            $next_item = $active_item.next('scroll-view');
-            $prev_item = $active_item.prev('scroll-view');
-        }
+        //     $active_item = $(page.element).find('scroll-view.active');
+        //     $next_item = $active_item.next('scroll-view');
+        //     $prev_item = $active_item.prev('scroll-view');
+        // }
 
         return $.when(shopping.getProduct(args.id), services.shopping.getProductStock(args.id),
             shopping.getProductComments(args.id, 4))
